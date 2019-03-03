6a7ffc2610be8f2d1be2485b5dad2440712f4c32
Eleanor Post <eap9238@g.rit.edu>
2019-03-03 12:41:30 -0500

    Compote got!

---
+++ Assets/Compote.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compote : MonoBehaviour
+{
+    public GameObject compote;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Apple")
+        {
+            Instantiate(compote, gameObject.transform.position, Quaternion.identity);
+
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
