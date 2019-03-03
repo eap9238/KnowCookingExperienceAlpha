d2acb93ff0117c670d6de533bad7380fa24d11b8
Eleanor Post <eap9238@g.rit.edu>
2019-03-02 22:30:28 -0500

    a great evil has been done this day
    and I am PROUD

---
+++ Assets/HandControls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandControls : MonoBehaviour
+{
+    Vector3 lastPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPosition = Input.mousePosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position += ((Input.mousePosition - lastPosition) / 100);
+
+        lastPosition = Input.mousePosition;
+    }
+}
---
+++ Assets/clickManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log(UnityEngine.Input.GetJoystickNames());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OVRInput.Update();
+
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            Debug.Log("Boogie Woogie");
+
+            RaycastHit hit;
+
+            var cameraCenter = GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Screen.width / 2f, Screen.height / 2f, GetComponent<Camera>().nearClipPlane));
+
+            if (Physics.Raycast(cameraCenter, this.transform.forward, out hit, 1000))
+            {
+                var obj = hit.transform.gameObject;
+            }
+        }
+
+        
+    }
+
+    // OnClick
+    private void OnMouseDown()
+    {
+    }
+}
